be2198a0c7744ba54694cd0aacfc31afbd489337
cc <cernicchiarocarlo@gmail.com>
2024-11-01 09:54:43 +0000

    backbone of bt_manager logic

--- bt_cpp/src/bt_manager.cpp
+++ bt_cpp/src/bt_manager.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <cmath>
 
+#include <ros/package.h>
+
 using namespace BT;
 
 template <typename NodeType>
@@ -35,82 +37,110 @@ void BtRun(BT::NodeStatus &status, std::unique_ptr<BT::Tree> &tree) {
           }
 }
 
+
+
 int main(int argc, char** argv)
 {
+  /// variables initialization
+
+  // ROS init
   ros::init(argc, argv, "bt_cpp_node");
-
-  BT::NodeStatus status = BT::NodeStatus::IDLE;
-
-  std::string abs_repo_path = "/home/ros/catkin_ws/src/";
-  std::string relative_xml_fold_path = "bt_ace/bt_cpp/bt_xml/";
-  
-  // BehaviorTreeFactory factory;
-
-  std::unique_ptr<BT::BehaviorTreeFactory> factory;
-  std::string path;
   ros::NodeHandle nh;
-  // path = "/home/ros/catkin_ws/src/bt_ace/bt_cpp/bt_xml/iauv_girona1000_survey_circular_true.xml";
 
-  std::deque<std::string> stack;
+  // path init
+  // (TODO: make it dynamic) change this according to where you installed the package
+  std::string abs_repo_path = ros::package::getPath("bt_cpp");
+  std::string relative_xml_fold_path = "/bt_xml/";
+  std::string extension = ".xml";
+  std::string path;
 
-  // TODO: function to populate stack
-  std::string bt_exec = "BT_1.xml";
-  stack.push_back(bt_exec);
-  bt_exec = "BT_2.xml";
-  stack.push_back(bt_exec);
-  bt_exec = "BT_3.xml";
-  stack.push_back(bt_exec);
-  
-  
+  // BT init
+  BT::NodeStatus status = BT::NodeStatus::IDLE;
+  std::unique_ptr<BT::BehaviorTreeFactory> factory;
   factory = std::make_unique<BT::BehaviorTreeFactory>();
-  
-  // iauv girona1000 survey
+  std::unique_ptr<BT::Tree> tree;
+  std::string bt_id = "main_bt";
+  // initialize possible BT nodes
   registerCustomNode<IauvGirona1000Survey::PathRequest>(
       *factory, "PathRequest", nh);
-
-
-  // registerCustomNode<IauvGirona1000Survey::isPathClear>(
-  //     *factory, "isPathClear", nh);
-  
+  registerCustomNode<IauvGirona1000Survey::FollowPath>(
+      *factory, "FollowPath", nh);
   factory->registerNodeType<isPathClear>("isPathClear", "is_path_clear", nh);
-  // factory->registerNodeType<IauvGirona1000Survey::isPathClear>("isPathClear", "is_path_clear");
-  // registerSimpleCondition(
-  //         "isPathClear",
-  //         std::bind(IauvGirona1000Survey::FollowPath::isPathClear));
-
   PortsList say_something_ports = { InputPort<std::string>("message") };
   factory->registerSimpleAction("LogSequence", LogSequence, say_something_ports);
   factory->registerSimpleAction("LogFallback", LogFallback, say_something_ports);
 
-  // factory->registerNodeType<DummyNodes::SaySomething>("SaySomething");
-  registerCustomNode<IauvGirona1000Survey::FollowPath>(
-      *factory, "FollowPath", nh);
-   
   
-  // factory.registerNodeType<SaySomething>("SaySomething");
-  // factory.registerNodeType<ThinkWhatToSay>("ThinkWhatToSay");
-
-  // Parse the XML file and create a tree_ from it
-  std::unique_ptr<BT::Tree> tree;
-  std::string bt_id = "main_bt";
+  // stack  
+  std::deque<std::string> stack;
+  std::string bt_exec;
 
 
+  /// path population
+  // TODO: function to populate stack
+  bt_exec = "BT_1";
+  stack.push_back(bt_exec);
+  bt_exec = "BT_2";
+  stack.push_back(bt_exec);
+  bt_exec = "BT_3";
+  stack.push_back(bt_exec);
 
-    while(!stack.empty()) {
+  /// execute BT in stack to start mission
+  while(!stack.empty()) {
 
-        std::string xml_tree = stack.front();
-        stack.pop_front();
-        path = abs_repo_path + relative_xml_fold_path + xml_tree;
-        factory->registerBehaviorTreeFromFile(path);
-        tree = std::make_unique<BT::Tree>(
+    std::string xml_tree = stack.front();
+    stack.pop_front();
+    path = abs_repo_path + relative_xml_fold_path + xml_tree + extension;
+    factory->registerBehaviorTreeFromFile(path);
+    tree = std::make_unique<BT::Tree>(
             factory->createTree(bt_id));
-        BT::NodeStatus status = BT::NodeStatus::IDLE;
+    BtRun(status, tree);
 
+    tree = std::make_unique<BT::Tree>(
+                factory->createTree(bt_id));
+    status = BT::NodeStatus::IDLE;
+                    
+
+  }
+  std::cout<<"first stack executed" <<std::endl;
+  
+  /// go to check to LUMA
+  std::string xml_tree = "bt_goto_luma";
+  path = abs_repo_path + relative_xml_fold_path + xml_tree + extension;
+  factory->registerBehaviorTreeFromFile(path);
+  tree = std::make_unique<BT::Tree>(
+            factory->createTree(bt_id));
+    BtRun(status, tree);
+
+    tree = std::make_unique<BT::Tree>(
+                factory->createTree(bt_id));
+    status = BT::NodeStatus::IDLE;
+  std::cout<<"goto luma executed" <<std::endl;
+
+  /// according to LUMA reply, populate new stack
+  bt_exec = "BT_4";
+  stack.push_back(bt_exec);
+  bt_exec = "BT_5";
+  stack.push_back(bt_exec);
+  bt_exec = "BT_6";
+  stack.push_back(bt_exec);
+
+  /// execute BT in stack to finish the mission with the tasks assigned by luma
+  while(!stack.empty()) {
+      std::string xml_tree = stack.front();
+      stack.pop_front();
+      path = abs_repo_path + relative_xml_fold_path + xml_tree + extension;
+      factory->registerBehaviorTreeFromFile(path);
+      tree = std::make_unique<BT::Tree>(
+            factory->createTree(bt_id));
         BtRun(status, tree);
 
         tree = std::make_unique<BT::Tree>(
                     factory->createTree(bt_id));
-    }
+        status = BT::NodeStatus::IDLE;
+  }
+  std::cout<<"last stack executed" <<std::endl;
+
 
 
   return 0;
